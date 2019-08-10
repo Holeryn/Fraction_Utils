86208059000aaf875524500fefda4b9e7d10315d
Holeryn <53957646+Holeryn@users.noreply.github.com>
2019-08-10 03:31:22 +0200

    Una ennesima correzione giuro che l'ultima

--- ProgrammaDiEsempio.c
+++ ProgrammaDiEsempio.c
@@ -53,17 +53,3 @@ s:
 
     printf("\n%d/%d\n",result.numerator,result.denominator);
 }
-
-int mcd(int a, int b)
-{
-    while (a != b) {
-        if (a > b) {
-            a = a - b;
-        }
-        else {
-            b = b - a;
-        }
-    }
-
-    return a;
-}
