55331a28a9aa1c9bcfc0333452f32d3e8904fa48
Holeryn <53957646+Holeryn@users.noreply.github.com>
2019-08-10 03:27:37 +0200

    Update fraction_utils.c

--- fraction_utils.c
+++ fraction_utils.c
@@ -4,9 +4,17 @@ data: 10 Agosto 2019
 Ora: 03:02 di notte
 */
 
+
+/*################ DIRETTIVE PER IL PREPROCESSORE ################ */
 #include <stdio.h>
 #include "fraction_utils.h"
 
+/*################ FUNZIONI ################ */
+
+/************************************
+ * mcd : calcola l'mcd di a e b     *
+ * returns : int (l'mcd)            *
+ ************************************/
 int mcd(int a, int b)
 {
     while (a != b) {
@@ -21,11 +29,21 @@ int mcd(int a, int b)
     return a;
 }
 
+/************************************
+ * mcm : calcola l'mcm di a e b     *
+ * returns : int (l'mcm)            *
+ ************************************/
 int mcm(int a, int b)
 {
     return (a*b)/mcd(a,b);
 }
 
+/************************************
+ * sum:fraction: calcola la somma   *
+ * tra f1 e f2                      *
+ * returns: fraction (la somma tra  *
+ * le due frazioni)                 *
+ *************************************/
 fraction sum_fraction(fraction f1, fraction f2)
 {
     int mcm_d = mcm(f1.denominator, f2.denominator);
@@ -36,6 +54,12 @@ fraction sum_fraction(fraction f1, fraction f2)
     return f;
 }
 
+/************************************
+ * sottract_fraction: calcola la    *
+ * sottrazione tra due frazioni     *
+ * returns: fraction (la            *
+ * sottrazione tra le due frazioni) *
+ ************************************/
 fraction sottract_fraction(fraction f1, fraction f2)
 {
     int mcm_d = mcm(f1.denominator, f2.denominator);
@@ -46,6 +70,13 @@ fraction sottract_fraction(fraction f1, fraction f2)
     return f;
 }
 
+/************************************
+ * multiply_fraction: calcola la    *
+ * moltiplicazione tra due frazioni *
+ * returns: fraction (la            *
+ * moltiplicazione tra le due       *
+ * frazione)                        *
+ ************************************/
 fraction multiply_fraction(fraction f1, fraction f2)
 {
     fraction f;
@@ -55,6 +86,12 @@ fraction multiply_fraction(fraction f1, fraction f2)
     return f;
 }
 
+/************************************
+ * divide_fraction: calcola la      *
+ * divisione tra due frazioni.      *
+ * returns: fraction (la divisione  *
+ * le due frazioni)                 *
+ ************************************/
 fraction divide_fraction(fraction f1,fraction f2)
 {
     fraction f;
@@ -64,6 +101,11 @@ fraction divide_fraction(fraction f1,fraction f2)
     return f;
 }
 
+/************************************
+ * minimi_termini: riduci ai minimi *
+ * termini una frazione             *
+ * returns: nothing (void)          *
+ ************************************/
 fraction minimi_termini(fraction f)
 {
     fraction frac;
