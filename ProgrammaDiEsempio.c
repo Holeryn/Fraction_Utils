c968edf4d9ade24bd197e19bac3917a39846bff2
Holeryn <53957646+Holeryn@users.noreply.github.com>
2019-08-11 21:38:42 +0000

    Update ProgrammaDiEsempio.c

--- ProgrammaDiEsempio.c
+++ ProgrammaDiEsempio.c
@@ -7,7 +7,6 @@ int main(void)
     fraction s2; 
     fraction result;
 
-    int mcd_value;
     int type;
 
 s:
@@ -18,33 +17,25 @@ s:
         printf("Inserisci una frazione in questo formato x/y + z/q: ");
         scanf("%d/%d + %d/%d",&s.numerator,&s.denominator,&s2.numerator,&s2.denominator);
 
-        mcd_value = mcd(s.numerator,s.denominator);
-        s.numerator /= mcd_value;
-        s.denominator /= mcd_value;
+        s = minimi_termini(s);
         result = sum_fraction(s,s2);
     }else if(type == 1){
         printf("Inserisci una frazione in questo formato x/y - z/q: ");
         scanf("%d/%d - %d/%d",&s.numerator,&s.denominator,&s2.numerator,&s2.denominator);
 
-        mcd_value = mcd(s.numerator,s.denominator);
-        s.numerator /= mcd_value;
-        s.denominator /= mcd_value;
+        s = minimi_termini(s);
         result = sottract_fraction(s,s2);
     }else if(type == 2){
         printf("Inserisci una frazione in questo formato x/y * z/q: ");
         scanf("%d/%d * %d/%d",&s.numerator,&s.denominator,&s2.numerator,&s2.denominator);
 
-        mcd_value = mcd(s.numerator,s.denominator);
-        s.numerator /= mcd_value;
-        s.denominator /= mcd_value;
+        s = minimi_termini(s);
         result = multiply_fraction(s,s2);
     }else if(type == 3){
         printf("Inserisci una frazione in questo formato x/y / z/q: ");
         scanf("%d/%d / %d/%d",&s.numerator,&s.denominator,&s2.numerator,&s2.denominator);
 
-        mcd_value = mcd(s.numerator,s.denominator);
-        s.numerator /= mcd_value;
-        s.denominator /= mcd_value;
+        s = minimi_termini(s);
         result = divide_fraction(s,s2);
     }else{
         printf("inserisci un numero valido");
